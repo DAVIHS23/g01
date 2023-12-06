83dbc82edb89884a313c7de57eca33e95951dc88
gusdel09 <gus.delhees@stud.hslu.ch>
2023-12-06 19:38:05 +0100

    total population with female and male graph

--- pie.js
+++ pie.js
@@ -1,7 +1,7 @@
 function createPie(width, height) {
   var pie = d3.select("#pie")
-                  .attr("width", width)
-                  .attr("height", height);
+      .attr("width", width)
+      .attr("height", height);
 
   pie.append("g")
       .attr("transform", "translate(" + width / 2 + ", " + (height / 2 + 10) + ")")
@@ -19,60 +19,34 @@ function drawPie(data, currentYear) {
   var pie = d3.select("#pie");
 
   var arcs = d3.pie()
-               .sort((a,b) => {
-                 if (a.continent < b.continent) return -1;
-                 if (a.continent > b.continent) return 1;
-                 return a.population - b.population;
-               })
-               .value(d => d.population);
+      .value(d => d.births);
 
   var path = d3.arc()
-               .outerRadius(+pie.attr("height") / 2 - 50)
-               .innerRadius(0);
+      .outerRadius(+pie.attr("height") / 2 - 50)
+      .innerRadius(0);
 
   var yearData = data.filter(d => d.year === currentYear);
-  var continents = [];
-  for (var i = 0; i < yearData.length; i++) {
-    var continent = yearData[i].continent;
-    if (!continents.includes(continent)) {
-      continents.push(continent);
-    }
-  }
 
   var colorScale = d3.scaleOrdinal()
-                     .domain(continents)
-                     .range(["#ab47bc", "#7e57c2", "#26a69a", "#42a5f5", "#78909c"]);
+      .domain(yearData.map(d => d.continent))
+      .range(["#ab47bc", "#7e57c2", "#26a69a", "#42a5f5", "#78909c"]);
 
   var update = pie
-                 .select(".chart")
-                 .selectAll(".arc")
-                 .data(arcs(yearData));
+      .select(".chart")
+      .selectAll(".arc")
+      .data(arcs(yearData));
 
-  update
-    .exit()
-    .remove();
+  update.exit().remove();
 
-  update
-    .enter()
+  update.enter()
       .append("path")
       .classed("arc", true)
       .attr("stroke", "#dff1ff")
       .attr("stroke-width", "0.25px")
-    .merge(update)
+      .merge(update)
       .attr("fill", d => colorScale(d.data.continent))
       .attr("d", path);
 
   pie.select(".pie-title")
-      .text("Total population, " + currentYear);
+      .text("Total births, " + currentYear);
 }
-
-
-
-
-
-
-
-
-
-
-
